d773a6d7aa58d3f8c5e38d2db3c535235dd6711b
Илья Ермолович <nikail232323232323@gmail.com>
2020-10-21 22:53:29 +0300

    Remade ExtraTask 2.1 without JS.

--- extraTasks/Task 2.1/task1.js
+++
@@ -1,9 +0,0 @@
-function handleClick1() {
-  document.getElementById("dlg").style.cssText = "visibility: hidden";
-  document.getElementById("shadow").style.cssText = "display: none";
-}
-function handleClick2() {
-  document.getElementById("dlg").style.cssText =
-    "visibility: visible; z-index: 2";
-  document.getElementById("shadow").style.cssText = "display: block";
-}
