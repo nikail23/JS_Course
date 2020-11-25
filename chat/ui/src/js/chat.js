8cb6ea15d877b0b137860813a8307fa3aa55b5a5
Илья Ермолович <nikail232323232323@gmail.com>
2020-11-25 20:02:01 +0300

    Add add, edit, send, loadmore functions.

--- chat/ui/src/js/chat.js
+++ chat/ui/src/js/chat.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-shadow */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
@@ -22,8 +23,8 @@ class FilterConfig {
 }
 
 class MessageList {
-  constructor(messages) {
-    this._messages = messages.slice();
+  constructor() {
+    this._restore();
   }
 
   get user() {
@@ -32,6 +33,18 @@ class MessageList {
 
   set user(value) {
     this._user = value;
+  }
+
+  _save() {
+    localStorage.setItem('messages', JSON.stringify(this._messages));
+  }
+
+  _restore() {
+    this._messages = JSON.parse(localStorage.getItem('messages'));
+  }
+
+  getMessagesLength() {
+    return this._messages.length;
   }
 
   getPage(skip, top, filterConfig) {
@@ -119,11 +132,11 @@ class MessageList {
 
   static validate(message) {
     if (!(typeof (message.id) === 'string')
-    || !(typeof (message.author) === 'string')
-    || !(typeof (message.text) === 'string')
-    || !(message.createdAt instanceof Date)
-    || (message.isPersonal && !(typeof (message.isPersonal) === 'boolean'))
-    || (message.to && !(typeof (message.to) === 'string'))) {
+      || !(typeof (message.author) === 'string')
+      || !(typeof (message.text) === 'string')
+      || !(message.createdAt instanceof Date)
+      || (message.isPersonal && !(typeof (message.isPersonal) === 'boolean'))
+      || (message.to && !(typeof (message.to) === 'string'))) {
       return false;
     }
     return true;
@@ -149,6 +162,7 @@ class MessageList {
         message.to,
       );
       this._messages.push(newMessage);
+      this._save();
       return true;
     }
     return false;
@@ -161,6 +175,7 @@ class MessageList {
         unvalidatedMessages.push(message);
       }
     });
+    this._save();
     return unvalidatedMessages;
   }
 
@@ -171,11 +186,13 @@ class MessageList {
       if (this.user) {
         if (message.author === this.user) {
           this._messages.splice(index, 1);
+          this._save();
           return true;
         }
         return false;
       }
       this._messages.splice(index, 1);
+      this._save();
       return true;
     }
     return false;
@@ -186,8 +203,12 @@ class MessageList {
       sourceMessage.id = newMessage.id;
       sourceMessage.author = newMessage.author;
       sourceMessage.text = newMessage.text;
-      sourceMessage.isPersonal = newMessage.isPersonal;
-      sourceMessage.to = newMessage.to;
+      if (newMessage.isPersonal) {
+        sourceMessage.isPersonal = newMessage.isPersonal;
+      }
+      if (newMessage.to) {
+        sourceMessage.to = newMessage.to;
+      }
       sourceMessage.createdAt = newMessage.createdAt;
     }
     if (editedMessage.author === undefined) {
@@ -205,11 +226,13 @@ class MessageList {
         if (this.user) {
           if (editableMessage.author === this.user) {
             copy(editableMessage, editedMessage);
+            this._save();
             return true;
           }
           return false;
         }
         copy(editableMessage, editedMessage);
+        this._save();
         return true;
       }
       return false;
@@ -219,6 +242,7 @@ class MessageList {
 
   clear() {
     this._messages = [];
+    this._save();
   }
 }
 
@@ -243,18 +267,18 @@ class MessagesView {
       const infoString = `${message.isPersonal ? `Personal message to ${message.to}` : 'Common message'} from ${message.author},<br/> at ${message.createdAt.toLocaleString('en-US', options)}`;
       if (message.author === currentUser.name) {
         messagesHTML
-          += `<div class="commonSentMessage">
-          ${message.text}
+          += `<div class="commonSentMessage" id="${message.id}">
+          <input type="text" disabled value="${message.text}">
           <img class="imgMes1 delete" src="https://icon-library.com/images/deleted-icon/deleted-icon-18.jpg"/>
           <img class="imgMes1 edit" src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8a/OOjs_UI_icon_edit-ltr-progressive.svg/1024px-OOjs_UI_icon_edit-ltr-progressive.svg.png"/>
         </div>  
         <div class="info info2">${infoString}</div> `;
       } else {
         messagesHTML
-          += `<div class="commonComeMessage">
-          ${message.text}
-        </div>  
-        <div class="info info1">${infoString}</div> `;
+          += `<div class="commonComeMessage" id="${message.id}">
+          <input type="text" disabled value="${message.text}">
+          </div>  
+        <div class="info info1">${infoString}</div>`;
       }
     });
     container.innerHTML = messagesHTML;
@@ -269,10 +293,20 @@ class User {
 }
 
 class UserList {
-  constructor(users, activeUsers, currentUser) {
-    this._users = users;
-    this._activeUsers = activeUsers;
-    this._currentUser = currentUser;
+  constructor() {
+    this._restore();
+  }
+
+  _save() {
+    localStorage.setItem('users', JSON.stringify(this._users));
+    localStorage.setItem('activeUsers', JSON.stringify(this._activeUsers));
+    localStorage.setItem('currentUser', JSON.stringify(this._currentUser));
+  }
+
+  _restore() {
+    this._users = JSON.parse(localStorage.getItem('users'));
+    this._activeUsers = JSON.parse(localStorage.getItem('activeUsers'));
+    this._currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
   setCurrentUser(name) {
@@ -280,6 +314,7 @@ class UserList {
     if (activeUserIndex !== -1) {
       this._currentUser = this._activeUsers[activeUserIndex];
     }
+    this._save();
   }
 
   getCurrentUser() {
@@ -290,6 +325,7 @@ class UserList {
     if (this.getUserIndex(name) === -1) {
       this._users.push(new User(name, avatar));
     }
+    this._save();
   }
 
   setActiveUser(name) {
@@ -298,6 +334,7 @@ class UserList {
     if (userIndex !== -1 && activeUserIndex === -1) {
       this._activeUsers.push(this._users[userIndex]);
     }
+    this._save();
   }
 
   setPassiveUser(name) {
@@ -305,6 +342,7 @@ class UserList {
     if (activeUserIndex !== -1) {
       this._activeUsers.splice(activeUserIndex, 1);
     }
+    this._save();
   }
 
   deleteUser(name) {
@@ -312,6 +350,7 @@ class UserList {
     if (index !== -1) {
       this._users.splice(index, 1);
     }
+    this._save();
   }
 
   getUserIndex(name) {
@@ -360,7 +399,7 @@ class ActiveUsersView {
       if (user.name !== activeUser.name) {
         html += `<div class="user">
         <img src="${user.avatar}" alt="">
-        ${user.name}
+        <span>${user.name}</span>
         </div>`;
       }
     });
@@ -384,102 +423,113 @@ class HeaderView {
   }
 }
 
-const readyMessages = [
-  new Message(
-    '0',
-    'Hello User1!',
-    new Date(),
-    'User2',
-    false,
-  ),
+class HelpBoxView {
+  constructor(helpBoxId) {
+    this.helpBoxId = helpBoxId;
+  }
 
-  new Message(
-    '1',
-    'Hello!',
-    new Date(),
-    'User1',
-    false,
-  ),
-
-  new Message(
-    '2',
-    'Hello User2!',
-    new Date(),
-    'User1',
-    true,
-    'User2',
-  ),
-
-  new Message(
-    '3',
-    '...',
-    new Date(),
-    'User3',
-    false,
-  ),
-];
-const messageListString = localStorage.getItem('messages');
-let messageList;
-if (messageListString) {
-  messageList = new MessageList(JSON.parse(messageListString));
-  messageList._messages.forEach((message) => {
-    message.createdAt = new Date(message.createdAt);
-  });
-} else {
-  messageList = new MessageList(readyMessages);
+  display(to) {
+    // eslint-disable-next-line no-undef
+    const helpBox = document.getElementById(this.helpBoxId);
+    helpBox.innerHTML = `[to: ${to}]`;
+  }
 }
+
+function checkLocalStorage() {
+  const readyUsers = [
+    new User('User1', 'https://image.flaticon.com/icons/png/512/194/194938.png'),
+    new User('User2', 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png'),
+    new User('User3', 'https://cdn.iconscout.com/icon/free/png-256/avatar-380-456332.png'),
+    new User('User4', 'https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png'),
+  ];
+  const readyActiveUsers = [
+    new User('User1', 'https://image.flaticon.com/icons/png/512/194/194938.png'),
+    new User('User2', 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png'),
+    new User('User3', 'https://cdn.iconscout.com/icon/free/png-256/avatar-380-456332.png'),
+  ];
+  const readyCurrentUser = readyActiveUsers[0];
+  const readyMessages = [
+    new Message(
+      '0',
+      'Hello User1!',
+      new Date(),
+      'User2',
+      false,
+    ),
+
+    new Message(
+      '1',
+      'Hello!',
+      new Date(),
+      'User1',
+      false,
+    ),
+
+    new Message(
+      '2',
+      'Hello User2!',
+      new Date(),
+      'User1',
+      true,
+      'User2',
+    ),
+
+    new Message(
+      '3',
+      '...',
+      new Date(),
+      'User3',
+      false,
+    ),
+  ];
+
+  if (localStorage.getItem('users') === null) {
+    localStorage.setItem('users', JSON.stringify(readyUsers));
+  }
+
+  if (localStorage.getItem('activeUsers') === null) {
+    localStorage.setItem('activeUsers', JSON.stringify(readyActiveUsers));
+  }
+
+  if (localStorage.getItem('currentUser') === null) {
+    localStorage.setItem('currentUser', JSON.stringify(readyCurrentUser));
+  }
+
+  if (localStorage.getItem('messages') === null) {
+    localStorage.setItem('messages', JSON.stringify(readyMessages));
+  }
+}
+
+checkLocalStorage();
+
+const messageList = new MessageList();
 const messagesView = new MessagesView('messages');
 
-const users = [
-  new User('User1', 'https://image.flaticon.com/icons/png/512/194/194938.png'),
-  new User('User2', 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png'),
-  new User('User3', 'https://cdn.iconscout.com/icon/free/png-256/avatar-380-456332.png'),
-  new User('User4', 'https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png'),
-];
-const activeUsers = [
-  new User('User1', 'https://image.flaticon.com/icons/png/512/194/194938.png'),
-  new User('User2', 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png'),
-  new User('User3', 'https://cdn.iconscout.com/icon/free/png-256/avatar-380-456332.png'),
-];
-
-const usersListString = localStorage.getItem('users');
-const activeUsersListString = localStorage.getItem('activeUsers');
-const currentUserString = localStorage.getItem('currentUser');
-
-let userList;
-if (usersListString !== 'null' && activeUsersListString !== 'null' && currentUserString !== 'null') {
-  userList = new UserList(JSON.parse(usersListString), JSON.parse(activeUsersListString), JSON.parse(currentUserString));
-} else {
-  userList = new UserList(users, activeUsers, activeUsers[0]);
-}
+const userList = new UserList();
 const activeUsersView = new ActiveUsersView('usersList');
+
+const helpBoxView = new HelpBoxView('mymessage');
 
 function addMessage(text, isPersonal, to) {
   if (messageList.add(new Message(undefined, text, undefined, undefined, isPersonal, to))) {
     this.showMessages();
-    return true;
   }
-  return false;
 }
 
 function editMessage(id, text, isPersonal, to) {
   if (messageList.edit(id, new Message(undefined, text, undefined, undefined, isPersonal, to))) {
     this.showMessages();
-    return true;
   }
-  return false;
 }
 
 function removeMessage(id) {
   if (messageList.remove(id)) {
     this.showMessages();
-    return true;
   }
-  return false;
 }
 
-function showMessages() {
-  messagesView.display(messageList.getPage(), userList.getCurrentUser());
+function showMessages(skip, top, filter) {
+  messagesView.display(messageList.getPage(skip, top, filter), userList.getCurrentUser());
 }
 
 function showActiveUsers() {
@@ -498,26 +548,123 @@ function setCurrentUser(name, avatar) {
   this.showActiveUsers();
 }
 
+function showHelpBox(to) {
+  helpBoxView.display(to);
+}
+
 // CONTROLLER
 
+let currentTop = 10;
+let currentSkip = 0;
+let currentFilter = null;
+let currentSelectedUser = 'All';
+
+showHelpBox(currentSelectedUser);
 showActiveUsers();
-showMessages();
+showMessages(currentSkip, currentTop, currentFilter);
 setCurrentUser('User1', 'https://image.flaticon.com/icons/png/512/194/194938.png');
 
-const messageInput = document.getElementById('sendButton');
-messageInput.addEventListener('click', (event) => {
-  event.preventDefault();
-  const textInput = document.getElementById('myInput');
-  const messageText = textInput.value;
-  if (messageText.length === 0) {
-    return;
-  }
-  if (addMessage(messageText, false)) {
-    textInput.value = '';
-  }
-});
+function addSelectUserEvent() {
+  const activeUsers = document.getElementsByClassName('user');
+  Array.prototype.slice.call(activeUsers).forEach((user) => {
+    user.addEventListener('click', (event) => {
+      const oldSelectedUser = currentSelectedUser;
+      currentSelectedUser = user.children[1].innerText;
+      if (oldSelectedUser === currentSelectedUser) {
+        currentSelectedUser = 'All';
+      }
+      const helpContainer = document.getElementById('mymessage');
+      showHelpBox(currentSelectedUser);
+    });
+  });
+}
 
-localStorage.setItem('users', JSON.stringify(userList.getAllUsers()));
-localStorage.setItem('activeUsers', JSON.stringify(userList.getAllActiveUsers()));
-localStorage.setItem('messages', JSON.stringify(messageList._messages));
-localStorage.setItem('currentUser', JSON.stringify(userList.getCurrentUser()));
+function addLoadOtherMessagesButtonEvent() {
+  const loadOtherMessagesButton = document.getElementById('loadmore');
+  loadOtherMessagesButton.addEventListener('click', (event) => {
+    if (currentTop < messageList.getMessagesLength()) {
+      currentTop += 10;
+    }
+    showMessages(currentSkip, currentTop, currentFilter);
+  });
+}
+
+function addDeleteEventToAllMessages() {
+  const deleteMessageButtons = document.getElementsByClassName('delete');
+  Array.prototype.slice.call(deleteMessageButtons).forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const messageContainer = button.parentNode;
+      removeMessage(messageContainer.id);
+      addEditEventToAllMessages();
+      addDeleteEventToAllMessages();
+    });
+  });
+}
+
+function addEditEventToAllMessages() {
+  const editMessageButtons = document.getElementsByClassName('edit');
+  Array.prototype.slice.call(editMessageButtons).forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const messageContainer = button.parentNode;
+      const messageInput = messageContainer.children[0];
+      const messageText = messageInput.value;
+
+      const editButton = messageContainer.children[1];
+      const deleteButton = messageContainer.children[2];
+
+      messageContainer.removeChild(editButton);
+      messageContainer.removeChild(deleteButton);
+      messageInput.removeAttribute('disabled');
+
+      messageInput.addEventListener('keydown', (event) => {
+        switch (event.keyCode) {
+          case 13:
+            editMessage(messageContainer.id, messageInput.value);
+            addDeleteEventToAllMessages();
+            addEditEventToAllMessages();
+            messageContainer.appendChild(deleteButton);
+            messageContainer.appendChild(editButton);
+            messageInput.setAttribute('disabled', true);
+            break;
+          case 27:
+            messageInput.value = messageText;
+            messageContainer.appendChild(deleteButton);
+            messageContainer.appendChild(editButton);
+            messageInput.setAttribute('disabled', true);
+            break;
+          default:
+            break;
+        }
+      });
+    });
+  });
+}
+
+function addSendButtonEvent() {
+  const messageInput = document.getElementById('sendButton');
+  messageInput.addEventListener('click', (event) => {
+    event.preventDefault();
+    const textInput = document.getElementById('myInput');
+    const messageText = textInput.value;
+    if (messageText.length === 0) {
+      return;
+    }
+    if (currentSelectedUser !== 'All') {
+      addMessage(messageText, true, currentSelectedUser);
+    } else {
+      addMessage(messageText, false);
+    }
+
+    addEditEventToAllMessages();
+    addDeleteEventToAllMessages();
+    textInput.value = '';
+    const messagesContainer = document.getElementById('messages');
+    messagesContainer.scrollTo(0, document.body.scrollHeight);
+  });
+}
+
+addSelectUserEvent();
+addLoadOtherMessagesButtonEvent();
+addSendButtonEvent();
+addDeleteEventToAllMessages();
+addEditEventToAllMessages();
